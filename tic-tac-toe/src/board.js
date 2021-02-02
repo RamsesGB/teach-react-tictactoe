5cfa7a5064ce8f5e61ec7572638ce0281ee3e403
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2021-02-02 18:27:27 -0500

    refactoring board component to hold the state of the squares

--- tic-tac-toe/src/board.js
+++ tic-tac-toe/src/board.js
@@ -3,8 +3,20 @@ import Square from './square.js';
 import './index.css';
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
