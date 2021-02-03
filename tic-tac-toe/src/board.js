ef4ef91356c36b80bca978896371daeac8542e4d
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2021-02-03 18:56:10 -0500

    lifting state oup from board into Game component

--- tic-tac-toe/src/board.js
+++ tic-tac-toe/src/board.js
@@ -3,14 +3,6 @@ import Square from './square.js';
 import './index.css';
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    };
-  }
-
   handleClick(i) {
     const squares = this.state.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -25,21 +17,13 @@ class Board extends React.Component {
   renderSquare(i) {
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     );
   }
 
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = 'Winner! ' + winner;
-    } else {
-      status = this.state.xIsNext ? 'Next player: X' : 'Next player: O';
-    }
-
     return (
       <div>
         <div className="status">{status}</div>
@@ -61,26 +45,6 @@ class Board extends React.Component {
       </div>
     );
   }
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
 }
 
 export default Board;
